9720b05dd199a6d966bdc27053d37819c280f02e
car35 <3932@holbertonschool.com>
2021-10-03 21:30:53 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
-
+/**
+ * main imprime un texto 
+ *
+va a retornar :siempre 0 cuando este OK
+*/
 
 int main( void )
 
 {
   
-  puts( "Programming is like building a multilingual puzzle\n" );
+  puts( "Programming is like building a multilingual puzzle");
   
   return(0);
   
