e4e8c77be298b5e99c09abf0cd57c72073f0dbaa
Carlos Arturo Ledezma Chaverra <86532190+car35@users.noreply.github.com>
2021-10-04 00:21:48 -0500

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,26 +1,20 @@
 #include <stdio.h>
-
-
-int main()
-  
+/**
+  * main - Prints the sizeof many var types
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void) 
 {
-  
   int h;
-  
   char o;
-  
   float f;
-  
   double l;
    
   printf("Size of int is: %ld\n",sizeof(h));
-  
   printf("Size of char is: %ld\n",sizeof(o));
-  
   printf("Size of float is: %ld\n",sizeof(f));
-  
   printf("Size of double is: %ld\n",sizeof(l));
   
   return 0;
-  
 }
