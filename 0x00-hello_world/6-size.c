87d66e98617b6d3c7f9a753fb8b04b828a8d1db8
car35 <3932@holbertonschool.com>
2021-10-03 22:17:56 -0700

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+int main()
+  
+{
+  
+  int h;
+  
+  char o;
+  
+  float f;
+  
+  double l;
+   
+  printf("Size of int is: %ld\n",sizeof(h));
+  
+  printf("Size of char is: %ld\n",sizeof(o));
+  
+  printf("Size of float is: %ld\n",sizeof(f));
+  
+  printf("Size of double is: %ld\n",sizeof(l));
+  
+  return 0;
+  
+}
