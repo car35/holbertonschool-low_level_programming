b3bd10d58ccf5e6c051c88297387d3f5c0bba093
Carlos Arturo Ledezma Chaverra <86532190+car35@users.noreply.github.com>
2021-10-04 22:40:56 -0500

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 /**
- * main - main function manages to display the alphabet backwards from (z) to (a)
- * Return:always (0).
+ * main -main function manages to display the alphabet backwards from (z) to (a)
+ * Return:always(0).
  **/
 int main(void)
 {
 char Alf;
 Alf = 'z';
-
 while (Alf >= 'a')
 {
 putchar(Alf);
