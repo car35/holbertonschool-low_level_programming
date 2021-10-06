3037002eac23b0ee5757ee119e5c4484d64fac23
Carlos Arturo Ledezma Chaverra <86532190+car35@users.noreply.github.com>
2021-10-05 22:13:09 -0500

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,28 @@
-#include <stdio.h>
-#include "main.h"
-
-/**main -funtion main imprimi la palabra putchar.
- *
- * return: devuelve el valor cero siempre
-
- */
-int main(){
-   
-  char Tx[8]="_putchar\n";
-  
-  int i=0;
- 
-  for(i=0;i<8;i++){
-    
-  putchar(Tx[i]);
-      
-  }
-  
-  return 0;
-   
+#include "main.h"                                                                                                                                                                               
+                                                                                                                                                                                                
+                                                                                                                                                                                                
+/**                                                                                                                                                                                             
+                                                                                                                                                                                                
+                                                                                                                                                                                                
+ * main -funtion main imprimi la palabra putchar                                                                                                                                                
+ *                                                                                                                                                                                              
+ * return: devuelve el valor cero siempre                                                                                                                                                       
+                                                                                                                                                                                                
+ */                                                                                                                                                                                             
+                                                                                                                                                                                                
+                                                                                                                                                                                                
+int main(){                                                                                                                                                                                     
+                                                                                                                                                                                                
+  char Tx[8]="_putchar\n";                                                                                                                                                                      
+                                                                                                                                                                                                
+  int i=0;                                                                                                                                                                                      
+                                                                                                                                                                                                
+  for(i=0;i<8;i++){                                                                                                                                                                             
+                                                                                                                                                                                                
+    putchar(Tx[i]);                                                                                                                                                                             
+                                                                                                                                                                                                
+  }                                                                                                                                                                                             
+                                                                                                                                                                                                
+  return 0;                                                                                                                                                                                     
+                                                                                                                                                                                                
 }
