abd337f97147cc886175d4b5e5c45e72980fe21e
Carlos Arturo Ledezma Chaverra <86532190+car35@users.noreply.github.com>
2021-10-05 22:39:01 -0500

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,27 +1,17 @@
-#include "main.h"                                                                                                                                                                               
-                                                                                                                                                                                                
-                                                                                                                                                                                                
-/**                                                                                                                                                                                                                                                                                                           
- * main -funtion main imprimi la palabra putchar                                                                                                                                                
- *                                                                                                                                                                                              
- * return: devuelve el valor cero siempre                                                                                                                                                       
-                                                                                                                                                                                                
- */                                                                                                                                                                                             
-                                                                                                                                                                                                
-int main(){                                                                                                                                                                                     
-                                                                                                                                                                                                
-  char Tx[11]="_putchar";                                                                                                                                                                      
-                                                                                                                                                                                                
-  int i=0;                                                                                                                                                                                      
-                                                                                                                                                                                                
-  for(i=0;i<11;i++){                                                                                                                                                                             
-                                                                                                                                                                                                
-    _putchar(Tx[i]); 
-    _putchar("\n");
-    _putchar("\n");
-                                                                                                                                                                                                
-  }                                                                                                                                                                                             
-                                                                                                                                                                                                
-  return 0;                                                                                                                                                                                     
-                                                                                                                                                                                                
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char T[10] = "_putchar\n";
+int i = 0;
+
+for (i = 0; i < 10; i++)
+{
+_putchar(T[i]);
+}
+return (0);
 }
