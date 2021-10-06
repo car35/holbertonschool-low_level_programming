a3049163b2c290516270bdf24241ea10444a95c1
car35 <3932@holbertonschool.com>
2021-10-06 12:04:23 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet.
+ */
+
+void print_alphabet(void)
+{
+  int x;
+  for ( x = 97; x <= 122; x++)
+  {
+    _putchar (x);
+    _putchar('\n');
+  }
+}
