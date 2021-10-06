0e1ca14d909e0a6fd80c7e25ca853942da8fd472
car35 <3932@holbertonschool.com>
2021-10-06 13:23:52 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* void print_alphabet_x10 - prints alphabet.
+*/
+void print_alphabet_x10(void)
+{
+int x, filas;
+for (filas = 0; filas < 10; filas++)
+{
+for (x = 97; x < 122; x++)
+{
+_putchar (x);
+}
+_putchar ('\n');
+}
+}
